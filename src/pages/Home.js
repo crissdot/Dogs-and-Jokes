c2626cd1c849de9f17c5b5681f4487c0cb1d8b58
crissdot <cris-2-11-2001@hotmail.com>
2021-01-25 11:22:24 -0600

    add show punchline

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,19 @@ import getData from '../utils/getData';
 const Home = async () => {
     const data = await getData();
 
+    window.showPunchline = () => {
+        const showPunch = document.querySelector('.show-punch');
+        const punchline = document.querySelector('.punchline');
+        showPunch.style.display = 'none';
+        punchline.style.display = 'block';
+    }
+
     const view = `
         <div class="characters">
             <article class="character-item">
                 <img src="${data.url}" alt="dog">
                 <h2 class="setup">${data.setup}</h2>
+                <p class="show-punch" onclick="showPunchline()">Show Punchline</p>
                 <h2 class="punchline">${data.punchline}</h2>
             </article>
         </div>
